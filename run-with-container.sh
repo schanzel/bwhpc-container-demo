2966bb105ec2a426c67304c9201744df216b3c69
Benjamin Schanzel <benjamin.schanzel@uni-ulm.de>
2018-09-19 20:38:10 +0200

    Fixed Singularity Cache Dir

--- run-with-container.sh
+++ run-with-container.sh
@@ -2,8 +2,10 @@
 #
 #PBS -N demo_with_container
 #
+export SINGULARITY_CACHEDIR="/beegfs/work/.singularity"
 
 module load devel/singularity/2.5.2
+
 singularity exec --nv \
 	shub://schanzel/tensorflow-keras-py3:gpu \
 	python ./work/bwhpc-container-demo/binary_classifier_lstm.py
